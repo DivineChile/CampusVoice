0d56b26c3a722cd709e7cbeddbabe7765ee90def
Divine Chile <divinechile16@gmail.com>
2026-03-17 18:15:56 +0100

    Finished Profile page and backend logic

---
+++ types/studentProfile.ts
@@ -0,0 +1,11 @@
+export interface StudentProfile {
+  fullName: string;
+  matricNumber: string;
+  email: string;
+  department: string;
+  level: string;
+  admissionYear: string;
+  role: string;
+  accountCreated: string;
+  initials: string;
+}
